d5b00f8d29da074acd3c9d383b3635bd2e74fa86
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-08-11 17:20:33 +0200

    method post success but trouble with the post of backend undefined

--- src/pages/AddHero.js
+++ src/pages/AddHero.js
@@ -1,31 +1,100 @@
 import { useEffect, useState } from "react";
 
 const AddHero = () => {
-  useEffect(() => {
-    setValue();
-  }, []);
+  const [slugValue, setSlugValue] = useState("");
+  const [nameValue, setNameValue] = useState("");
+  const [power, setPower] = useState([]);
+  const [colorValue, setColorValue] = useState("");
+  const [aliveValue, setAliveValue] = useState("");
+  const [ageValue, SetAgeValue] = useState("");
 
-  const [value, setValue] = useState("");
-  const handleSubmit = (e) => {
+  //   useEffect(() => {
+  //     getValueFromInput();
+
+  //   }, []);
+
+  const getValueFromInputSlug = (e) => {
+    setSlugValue(e.target.value);
+  };
+
+  const getValueFromInputName = (e) => {
+    setNameValue(e.target.value);
+  };
+
+  const getValueFromPowerInput = (e) => {
+    const clonedPower = [...power, e.target.value];
+    setPower(clonedPower);
+  };
+
+  const getColorValueFromInput = (e) => {
+    setColorValue(e.target.value);
+  };
+
+  const getAliveValueFromInput = (e) => {
+    setAliveValue(e.target.value);
+  };
+
+  const getAgeValueFromInput = (e) => {
+    SetAgeValue(e.target.value);
+  };
+  console.log(slugValue, nameValue, colorValue, aliveValue, ageValue, power);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const newObj = {
+      slug: slugValue,
+      name: nameValue,
+      power: power,
+      color: colorValue,
+      isAlive: aliveValue,
+      age: ageValue,
+      image:
+        "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg",
+    };
+
+    const request = await fetch(`http://localhost:5000/heroes/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newObj),
+    });
+
+    const response = await request.json();
   };
 
-  const getValueFromInput = (e) => {
-    setValue(e.target.value);
-    const newObj = {
-      slug: value,
-      name: 
-    };
-  };
   return (
     <div>
       <h2>Add</h2>
-      <form action="" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Slug" onChange={getValueFromInput} />
-        <input type="text" placeholder="Name" onChange={getValueFromInput} />
-        <input type="text" placeholder="" onChange={getValueFromInput} />
-        <input type="text" placeholder="" onChange={getValueFromInput} />
-        <input type="text" placeholder="" onChange={getValueFromInput} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Slug"
+          onChange={getValueFromInputSlug}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={getValueFromInputName}
+        />
+        <input
+          type="text"
+          placeholder="power"
+          onChange={getValueFromPowerInput}
+        />
+        <input
+          type="text"
+          placeholder="color"
+          onChange={getColorValueFromInput}
+        />
+        <input
+          type="text"
+          placeholder="isAlive"
+          onChange={getAliveValueFromInput}
+        />
+        <input type="text" placeholder="age" onChange={getAgeValueFromInput} />
+        <button type="submit">btn</button>
       </form>
     </div>
   );
