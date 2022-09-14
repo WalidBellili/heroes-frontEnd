45a47589fb04f43f0b37c610805236150b579ca2
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-14 14:56:59 +0200

    fetch work, props for card too

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Card = ({ hero }) => {
+  console.log(hero);
+  return <div></div>;
+};
+
+export default Card;
--- src/pages/Heros.js
+++ src/pages/Heros.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
 import Header from "../components/Header";
 
 const Heros = () => {
+  const [heroes, setHeroes] = useState([]);
+  useEffect(() => {
+    fetchHeroes();
+  }, []);
+
+  const fetchHeroes = async () => {
+    const request = await fetch(`http://localhost:5000/heroes`);
+    const response = await request.json();
+    setHeroes(response);
+  };
+  // console.log(heroes);
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      {heroes.map((hero) => {
+        return <Card hero={hero} />
+      })}
+    </>
   );
 };
 
