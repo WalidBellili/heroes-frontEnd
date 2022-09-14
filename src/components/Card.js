331aefff913629f144f553b757104dea7c73b16b
Walid Bellili <walid.bellili.seixas@gmail.com>
2022-09-14 15:01:03 +0200

    i had to make a map in the card to display the powers in an array

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const Card = ({ hero }) => {
   console.log(hero);
-  return <div></div>;
+  return (
+    <>
+      <img src={hero.image} alt={hero.name} />
+      <p>{hero.name}</p>
+      <p>{hero.age}</p>
+
+      {hero.power.map((p) => {
+        return <li>{p}</li>;
+      })}
+    </>
+  );
 };
 
 export default Card;
